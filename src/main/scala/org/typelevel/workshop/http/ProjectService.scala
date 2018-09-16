40966911e1b8c3cfe26bd1b117c3c939bdf8e75a
Stefano Tondo <stondo@gmail.com>
2018-09-17 00:23:23 +0200

    added findAll to ProjectRepository/Service. gitignored .idea/

--- src/main/scala/org/typelevel/workshop/http/ProjectService.scala
+++ src/main/scala/org/typelevel/workshop/http/ProjectService.scala
@@ -8,7 +8,10 @@ import org.http4s.dsl.io._
 import org.http4s.circe.CirceEntityCodec._
 import org.typelevel.workshop.repository.ProjectRepository
 
+
 object ProjectService {
+
+  case class UpdateNameAndDescProject(id: Int, name: String, description: String)
 
   val service: HttpService[IO] = HttpService[IO] {
 
@@ -17,6 +20,21 @@ object ProjectService {
         case Some(project) => Ok(project)
         case None => NotFound(s"No project found: $name".asJson)
       }
+
+    case GET -> Root =>
+      ProjectRepository.findAll().flatMap {
+        case list => println(list); Ok(list)
+        case Nil => NotFound(s"No project found: ".asJson)
+      }
+
+    case req @ PUT -> Root => for {
+      updateNameDesc <- req.as[UpdateNameAndDescProject]
+      projectOption <- ProjectRepository.updateNameAndDesc(updateNameDesc.id, updateNameDesc.name, updateNameDesc.description)
+      result <- projectOption match {
+        case affectedRows => Created(affectedRows)
+        case -1 => BadRequest("Project can't be updated".asJson)
+      }
+    } yield result
 
     case req @ DELETE -> Root / name =>
       ProjectRepository.deleteProject(name).flatMap(_ => NoContent())
--- src/main/scala/org/typelevel/workshop/repository/ProjectRepository.scala
+++ src/main/scala/org/typelevel/workshop/repository/ProjectRepository.scala
@@ -1,11 +1,15 @@
 package org.typelevel.workshop.repository
 
-import org.typelevel.workshop.model.Project
 import cats.effect.IO
 import cats.implicits._
 import doobie.implicits._
+import io.circe.generic.auto._
+import org.http4s.circe._
+import org.typelevel.workshop.model.Project
 
 object ProjectRepository {
+
+//  implicit val decoder = jsonOf[IO, Project]
 
   def findByName(name: String): IO[Option[Project]] =
     sql"""
@@ -14,6 +18,20 @@ object ProjectRepository {
       WHERE p.name = $name
     """.query[Project].option.transact(Database.xa)
 
+  def findAll(): IO[List[Project]] = {
+    sql"""
+      SELECT p.id, p.name, p.description, u.id, u.username, u.email FROM project p JOIN user u ON p.owner = u.id
+    """.query[Project].to[List].transact(Database.xa)
+  }
+
+  def updateNameAndDesc(id: Int, name: String, description: String): IO[Int] =
+    sql"""
+      UPDATE project SET name = $name, description = $description
+      WHERE id = $id
+    """
+      .update
+      .run
+      .transact(Database.xa)
 
   def deleteProject(name: String): IO[Unit] = (for {
     projectId <- sql"SELECT id FROM project WHERE name = $name".query[Int].unique
